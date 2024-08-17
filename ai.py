c777080b5bf3b558f2dfe2b48ed8c9efea4c9f60
Manansh Arora <manansh4coding@gmail.com>
2024-08-17 14:58:20 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -7,6 +7,9 @@ import csv
 import wikipedia
 import smtplib
 import AppOpener
+import regex #email checker
+import re
+import sys
 
 def wishme(name):
     hour = int(datetime.datetime.now().hour)
@@ -32,7 +35,7 @@ def input_voice():
     r = sr.Recognizer()
     with sr.Microphone as source:
         print("Listening....")
-        r.pause_threshold = 1 
+        r.pause_threshold = 0.5
         audio = r.listen(source)
 
     try:
@@ -56,42 +59,60 @@ def sendEmail(recipient, text):
 
 def acc_check():
     try:
-        with open("data.csv" , "r", newline= '') as file:
+        with open("data.csv" , "r",) as file:
             reader1 = csv.DictReader(file)
-            rows1 = [r for r in reader1]
-            lastrow1 = rows1[-1]
-            name = lastrow1[-1]
-    except Exception:
+            row1 = [r for r in reader1]
+            last_row = row1[-1]
+            name = last_row["Name"]
+            wishme(name)
+            speak("Enter Query Here:\n")
+
+    except (Exception , IndexError):
         speak("No user found , Creating Account")
         speak("Please tell me your name")
-        #name = input_voice()
-        name = input("name ")
+        # name = input_voice()
+        name = input("name: ")
         speak(f"Hello, {name} ,please enter your email, you're gonna have to type this one.")
-        email = input("Enter Email here")
+        email = input("Enter Email here: ")
+        if input_email(email):
+                   pass
+        else:
+             sys.exit()
         data.create_info(name , email)
         speak("Info created , please enter query")
 
+def input_email(email):
+     if data.validate_email(email):
+          speak("Email Created")
+          return True
+     else:
+          return False
+                    
 acc_check()
 
 
 while True:
     try:
-       query = input("query")
-       #query = input_voice.lower()
+       query = input("query: ")
+    #    query = input_voice
     except Exception as e:
          speak(f"Uh oh, {e} found")
          break
-    if "create profile" in query:
+    if "create" and ("profile" or "account") in query:
          with open("data.csv" , "r") as file:
-              reader = csv.DictReader(file)
+              '''reader = csv.DictReader(file)
               rows = [r for r in reader]
               lastrow = rows[-1]
               wishme(lastrow["Name"])
-              speak("Please tell me your name")
-              #name = input_voice()
-              name = input("name ")
-              speak(f"Hello, {name} ,pls enter your email, you're gonna have to type this one.")
+              speak("Please tell me your name")'''
+            #   name = input_voice()
+              name = input("Name: ")
+              speak(f"Hello {name} ,please enter your email, you're gonna have to type this one.")
               email = input("Enter Email here")
+              if input_email(email):
+                   pass
+              else:
+                   break
               data.create_info(name , email)
               speak(f"Hello {name}, profile succesfully created , pls proceed with further queries ")
     elif "open google" in query:
@@ -116,7 +137,7 @@ while True:
         webbrowser.open("youtube.com")
     elif "the time" in query:
         strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-        speak(f"Sir, the time is {strTime}")
+        speak(f"According to the ITC, the time is {strTime}")
     elif "open discord" in query:
          speak("Opening Discord")
          AppOpener.open("discord")
@@ -127,4 +148,3 @@ while True:
          break
     else:
          speak("Unrecoganized command, Please try again")
-         

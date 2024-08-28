6d8901849131d22a186bdddbfca8d6b2272535b4
Avyakt Jain <avyakt06@gmail.com>
2024-08-28 18:47:41 +0530

    Update speak.py

--- speak.py
+++ speak.py
@@ -1,3 +1,10 @@
 import pyttsx3
 
-
+def speak(audio):
+    print(audio)
+    engine = pyttsx3.init('sapi5') #sapi5 is microsofts speech api , helps recoganize the speech input
+    voices= engine.getProperty('voices') 
+    engine.setProperty('voice', voices[0].id)
+    
+    engine.say(audio)
+    engine.runAndWait() #Makes the speech audible
